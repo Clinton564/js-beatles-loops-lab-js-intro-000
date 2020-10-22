09900cacaad8a6ad19946c3f72e1df2f8916625f
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 00:46:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesPlay = new Array()
   for(let i = 0; i < musicians.length; i++) {
-    console.log(musicians[i])
-    console.log(instruments[i])
     beatlesPlay.push(musicians[i] + " plays " + instruments[i])
   }
   return beatlesPlay
