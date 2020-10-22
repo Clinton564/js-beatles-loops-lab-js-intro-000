8e6b77aebd0801889941c300cb7b8bc05906eae9
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 00:33:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
+  var BeatlesPlay[];
   for(let i = 0; i < musicians.length; i++) {
     console.log(musicians[i])
     console.log(instruments[i])
+    BeatlesPlay.push(`${musicians[i]} plays the ${instruments[i]})
   }
 }
