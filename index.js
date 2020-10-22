66d094728599ef2d5b50090550265e5bddcb40ad
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 00:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  for(let i = 0; i < musicians.length(); i++) {
+  for(let i = 0; i < musicians.length; i++) {
     console.log(musicians[i])
     console.log(instruments[i])
   }
