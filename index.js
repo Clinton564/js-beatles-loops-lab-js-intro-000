d850a7665b799aa66b807017ae817725723d42b1
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 00:30:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  for(let i = 0; i < musicians.length(); i++) {
+    console.log(musicians[i])
+    console.log(instruments[i])
+  }
+}
